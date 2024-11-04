703bc31fb0bfa96359eb4822269fc63b7fafbe2e
Dylan Dominno <dylandominno7@gmail.com>
2024-11-04 19:59:49 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,9 @@
+// Collecting user information
+const subjectTitle = "Programming Languages"; // Replace with actual input
+const classSchedule = "3:00 Pm - 4:30 PM, Mon/Fri"; // Replace with actual input
+const classroom = "CHED122"; // Replace with actual input
+const classInstructor = "Sir Kim"; // Replace with actual input
+const studentName = "Dylan Dominno"; // Replace with actual input
+
+// Logging the information to the console
+console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.);
